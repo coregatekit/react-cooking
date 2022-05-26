caa358332207714b9fb42050e1164edd79d75c8c
Joe Kim <joesm.genie@gmail.com>
2022-05-27 00:25:39 +0700

    🎨 Implement page not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <div className="text-center mt-24">
+        <h1 className="text-8xl">404</h1>
+        <p className="text-4xl">Page not found</p>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
