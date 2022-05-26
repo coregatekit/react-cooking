4ad3c627b9d586f9411360f3f9a924f7f7ba561f
Joe Kim <joesm.genie@gmail.com>
2022-05-26 17:59:18 +0700

    🎨 Test dependencies workss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import { TextField } from "@mui/material";
 import React from "react";
 
 function Home() {
   return (
-    <div>
+    <div className="container mx-auto">
       <div className="text-xl3 font-bold underline">Hello World!</div>
+      <div>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </div>
     </div>
   );
 }
