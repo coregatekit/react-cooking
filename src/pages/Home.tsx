a7d2d43b76a3504690cc1298d394fb15d7c49289
Joe Kim <joesm.genie@gmail.com>
2022-06-16 16:04:48 +0700

    💄 Change alignment to center

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,29 +4,33 @@ import React, { useState } from "react";
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
 
-  const handleSearchQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearchQueryChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setSearchQuery(event.target.value);
     console.log(searchQuery);
-  }
+  };
 
   return (
     <div className="container mx-auto">
-        <div className="mt-40">
-          <div className="text-6xl text-left p-2">ค้นหาเมนูที่คุณต้องการ</div>
-          <div className="p-2">
-            <TextField
-              id="search-menu"
-              fullWidth
-              variant="standard"
-              placeholder="ผัดเปรี้ยวหวานหมูสับ..."
-              style={{ marginTop: 16}}
-              inputProps={{ style: {
+      <div className="mt-40">
+        <div className="text-6xl text-center p-2">ค้นหาเมนูที่คุณต้องการ</div>
+        <div className="p-2">
+          <TextField
+            id="search-menu"
+            fullWidth
+            variant="standard"
+            placeholder="ผัดเปรี้ยวหวานหมูสับ..."
+            style={{ marginTop: 16, width: "50%", alignContent: "left"}}
+            inputProps={{
+              style: {
                 fontSize: 40,
-              }}}
-              onInput={handleSearchQueryChange}
-            />
-          </div>
+              },
+            }}
+            onInput={handleSearchQueryChange}
+          />
         </div>
+      </div>
     </div>
   );
 }
