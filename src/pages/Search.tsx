308a6607a3a448cbff9a411fcf3c6e75a587882e
Joe Kim <joesm.genie@gmail.com>
2022-06-18 20:19:24 +0700

    🚧  Resdesign ui

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Search() {
   return (
-    <div>Search</div>
-  )
+    <div>
+      <div className="flex justify-between mt-16 mb-4">
+        <div>ค้นหาเมนูที่คุณต้องการ</div>
+        <div>Icon</div>
+      </div>
+      <div className="flex flex-row justify-center">
+        <div className="bg-green-400 basis-1/4 border-r-2 border-r-black p-2">
+          01
+        </div>
+        <div className="bg-red-400 basis-1/2">02</div>
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
