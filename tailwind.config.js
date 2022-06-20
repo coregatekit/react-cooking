f770a3fb1bdea15c3b4125f2c4e0b15491423c01
Joe Kim <joesm.genie@gmail.com>
2022-06-20 12:53:56 +0700

    ➕ Add tailwind material

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -6,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+});
