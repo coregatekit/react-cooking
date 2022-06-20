68c72c76fb4e503549aa04e60b49abc317d66e3b
Joe Kim <joesm.genie@gmail.com>
2022-06-20 17:20:12 +0700

    💄 Add button cover card

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -3,6 +3,7 @@ import {
   CardHeader,
   CardBody,
   Typography,
+  Button,
 } from "@material-tailwind/react";
 import React from "react";
 import { IState as IProps } from "../pages/Home";
@@ -11,8 +12,8 @@ export const MenuList: React.FC<IProps> = ({ menus }) => {
   const renderList = () => {
     return menus.map((menu) => {
       return (
-        <div className="p-6">
-          <Card className="w-96 mt-6">
+        <Button key={menu.id} variant="text">
+          <Card key={menu.id} className="w-96 mt-6">
             <CardHeader color="grey" className="relative h-56">
               <img src={menu.image} alt={menu.name} className="w-full h-full" />
             </CardHeader>
@@ -25,13 +26,13 @@ export const MenuList: React.FC<IProps> = ({ menus }) => {
               </Typography>
             </CardBody>
           </Card>
-        </div>
+        </Button>
       );
     });
   };
   return (
     <div className="flex justify-center">
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
         {renderList()}
       </div>
     </div>
